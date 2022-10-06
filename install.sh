2111e23803455479a67e5df0756b44e07486a158
landcat222 <67131672+landcat222@users.noreply.github.com>
2022-10-06 09:38:19 +0900

    zshの設定ファイルをバックアップディレクトリに移動する処理を追加

--- install.sh
+++ install.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 git clone https://github.com/landcat222/dotfiles.git ~/.dotfiles
 mkdir ~/.backup
+
+# zsh files backup
+if [ -e ~/.zshenv ];then
+  mv -b ~/.zshenv ~/.backup/zshenv
+fi
+
+if [ -e ~/.zshrc ];then
+  mv -b ~/.zshrc ~/.backup/zshrc
+fi
+
+if [ -e ~/.zprofile ];then
+  mv -b ~/.zprofile ~/.backup/zprofile
+fi
+
+if [ -e ~/.zlogin ];then
+  mv -b ~/.zlogin ~/.backup/zlogin
+fi
+
+if [ -e ~/.zlogout ];then
+  mv -b ~/.zlogout ~/.backup/zlogout
+fi
+
+if [ -e ~/.zsh ];then
+  mv -b ~/.zsh ~/.backup/zsh
+fi
